96e2b23ad70537b07ac1387e994469133a9727a5
Mizanur <jameslev2001@gmail.com>
2021-10-10 12:30:38 +0530

    first commit

---
+++ index.php
@@ -0,0 +1,124 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="image/x-icon" href="img/logo.png">
+    <title>Bhabta Azizia High Madrasah results section</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Baloo+Bhai+2&family=Bree+Serif&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/head_foot.css">
+    <link rel="stylesheet" media="screen and (max-width: 780px)" href="css/tab_head_foot.css">
+    <link rel="stylesheet" media="screen and (max-width: 460px)" href="css/phone_head_foot.css">
+    <style>
+      @media (max-width:340px)  {
+        #container {
+         margin: 5px;
+         overflow: auto;
+        }
+    }
+    </style>
+</head>
+
+<body>
+
+    <?php require "partial/header.php";?>
+    <div id="container">
+        <div class="box">
+            <img src="img/class5.jpg" alt="class5">
+            <div class="heading">
+                <h1>Class V</h1>
+                <div class="hyperlinks">
+                    <a href="under_construction.php">A</a>
+                    <a href="under_construction.php">B</a>
+                    <a href="under_construction.php">C</a>
+                </div>
+            </div>
+        </div>
+        <div class="box">
+            <img src="img/class6.jpg" alt="class6">
+            <div class="heading">
+                <h1>Class Vi</h1>
+                 <div class="hyperlinks">
+                    <a href="under_construction.php">A</a>
+                    <a href="under_construction.php">B</a>
+                    <a href="under_construction.php">C</a>
+                </div>
+            </div>
+        </div>
+        <div class="box">
+            <img src="img/class7.jpg" alt="class7">
+            <div class="heading">
+                <h1>Class Vii</h1>
+                 <div class="hyperlinks">
+                    <a href="under_construction.php">A</a>
+                    <a href="under_construction.php">B</a>
+                    <a href="under_construction.php">C</a>
+                </div>
+            </div>
+        </div>
+        <div class="box">
+            <img src="img/class8.jpg" alt="class8">
+            <div class="heading">
+                <h1>Class Viii</h1>
+                 <div class="hyperlinks">
+                    <a href="under_construction.php">A</a>
+                    <a href="under_construction.php">B</a>
+                    <a href="under_construction.php">C</a>
+                </div>
+            </div>
+        </div>
+        <div class="box">
+            <img src="img/class9.jpg" alt="class9">
+            <div class="heading">
+                <h1>Class iX</h1>
+                 <div class="hyperlinks">
+                    <a href="under_construction.php">A</a>
+                    <a href="teachers.php">B</a>
+                    <a href="under_construction.php">C</a>
+                </div>
+            </div>
+        </div>
+        <div class="box">
+            <img src="img/class10.jpg" alt="class10">
+            <div class="heading">
+                <h1>Class X</h1>
+                 <div class="hyperlinks">
+                    <a href="under_construction.php">A</a>
+                    <a href="under_construction.php">B</a>
+                    <a href="under_construction.php">C</a>
+                </div>
+            </div>
+        </div>
+        <div class="box">
+            <img src="img/class11.jpg" alt="class5">
+            <div class="heading">
+                <h1>Class Xi</h1>
+                 <div class="hyperlinks">
+                    <a href="under_construction.php">A</a>
+                    <a href="under_construction.php">B</a>
+                    <a href="under_construction.php">C</a>
+                </div>
+            </div>
+        </div>
+        <div class="box">
+            <img src="img/Class12.jpg" alt="class5">
+            <div class="heading">
+                <h1>Class Xii</h1>
+                 <div class="hyperlinks">
+                    <a href="under_construction.php">A</a>
+                    <a href="under_construction.php">B</a>
+                    <a href="under_construction.php">C</a>
+                </div>
+            </div>
+        </div>
+
+    </div>
+    <?php require "partial/footer.php";?>
+</body>
+
+</html>
